c2c9265352669244a727976baf439350719fcb64
Dridi Walid <wdridi1@gmail.com>
2017-12-19 23:01:16 +0100

    Refacto to kernel.php

--- public/index.php
+++ public/index.php
@@ -2,11 +2,35 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+use GuzzleHttp\Psr7\Response;
 use GuzzleHttp\Psr7\ServerRequest;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 
-$app = new \Framework\App();
+class Kernel
+{
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        $uri = $request->getUri()->getPath();
+       
+        if (!empty($uri) && $uri[-1] === "/") {
+            return (new Response())
+                ->withStatus(301)
+                ->withHeader('Location', substr($uri, 0, -1));
+                
+            return $response;
+        }
+        
+        if ($uri === '/blog') {
+            return new Response(200, [], '<h1>Bienvenue sur le blog</h1>');
+        }
+        
+        return new Response(404);
+    }
+}
 
-$response = $app->run(ServerRequest::fromGlobals());
-
+$kernel = new Kernel();
+$request = ServerRequest::fromGlobals();
+$response = $kernel->handle($request);
 \Http\Response\send($response);
 
