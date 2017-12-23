9b602c99009460ef006e9efeb9876eaa1dd54ebd
Dridi Walid <wdridi1@gmail.com>
2017-12-23 22:42:43 +0100

    #22 Refactor __invoke dependencies

--- tests/Middleware/XssProtectionTest.php
+++ tests/Middleware/XssProtectionTest.php
@@ -8,7 +8,11 @@ class XssProtectionTest extends TestCase
 {
     public function test_xss_protection_header()
     {
-        $xssProtectionMiddleware = (new \Middleware\XssProtection)(new \GuzzleHttp\Psr7\ServerRequest('GET', ''), new \GuzzleHttp\Psr7\Response);
+        $xssProtectionMiddleware = (new \Middleware\XssProtection)
+            (
+                new \GuzzleHttp\Psr7\ServerRequest('GET', ''), 
+                new \GuzzleHttp\Psr7\Response
+            );
         
         $this->assertEquals(
             $xssProtectionMiddleware->getHeader('X-XSS-Protection')[0], 
