17d86fbe9cf9d37f4cc6b454dbe8f1bf51eb13da
Dridi Walid <wdridi1@gmail.com>
2018-01-03 10:34:17 +0100

    Update CryptoTest.php

--- tests/Service/CryptoTest.php
+++ tests/Service/CryptoTest.php
@@ -6,6 +6,11 @@ use PHPUnit\Framework\TestCase;
 
 class HsmTest extends TestCase
 {
-    
+    $crypto = new \Service\Crypto(new \GuzzleHttp\Client());
+        
+        $this->assertEquals(
+            '020f61d1c4e647c1a5563bec87821a9c5c328292edbb782f86f34fac86039573', 
+            $crypto->createHash('sha256', 'micro_framework')
+        );
 }
 
